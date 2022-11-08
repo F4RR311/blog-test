c6c3659807f7981ac4042f3e9bb1a6160345f04b
F4RR311 <valery.leo2016@yandex.ru>
2022-11-08 22:58:19 +0300

    landing

---
+++ app.js
@@ -0,0 +1,25 @@
+const cols = document.querySelectorAll('.col');
+
+function generateRandomColor() {
+
+    const hexCodes = '0123456789ABCDEF';
+    let color = '';
+    for (let i = 0; i < 6; i++) {
+        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+
+    }
+    return '#' + color
+}
+
+function setRandomColors() {
+
+    cols.forEach(col => {
+        const text = col.querySelector('h2');
+        const colorText = generateRandomColor();
+
+        col.style.background = generateRandomColor()
+        text.textContent = colorText
+    })
+}
+
+setRandomColors()
